e9469f716443f17fcefe47ba8871ccee8bb7d1ff
tdurgapsd <tangududurga12@gmail.com>
2026-02-04 12:00:43 +0530

    add entities and repositories

---
+++ src/main/java/com/durga/blogapp/comments/CommentEntity.java
@@ -0,0 +1,38 @@
+package com.durga.blogapp.comments;
+
+import com.durga.blogapp.articles.ArticleEntity;
+import com.durga.blogapp.users.UserEntity;
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Null;
+import lombok.*;
+
+@Entity(name = "comments")
+@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class CommentEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(nullable = false)
+    private Long id;
+
+    @Nullable
+    private String title;
+
+    @NonNull
+    private String body;
+
+    @ManyToOne
+    @JoinColumn(name = "articleId", nullable = false)
+    private ArticleEntity article;
+
+    @ManyToOne
+    @JoinColumn(name = "authorId", nullable = false)
+    private UserEntity author;
+
+
+}
---
+++ src/main/java/com/durga/blogapp/users/UserEntity.java
@@ -0,0 +1,37 @@
+package com.durga.blogapp.users;
+
+import jakarta.annotation.Nullable;
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity(name = "users")
+@Data
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(nullable = false)
+    @NonNull
+    private String username;
+
+    @Column(nullable = false)
+    @NonNull
+    private String email;
+
+    @Column(nullable = true)
+    @Nullable
+    private String bio;
+
+    @Column(nullable = true)
+    @Nullable
+    private String image;
+
+
+}
